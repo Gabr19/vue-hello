9bcfe9ecf978d04297ad6e84648dfe8313e54e27
Gabr19 <sannagabriele19@gmail.com>
2023-01-20 22:49:27 +0100

    .

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,10 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      inputValue : '',
+      buttonLabel: 'invia',
+      link: 'https://cdn.pixabay.com/photo/2016/10/21/14/50/plouzane-1758197__480.jpg'
     }
   }
 }).mount('#app')
